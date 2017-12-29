90412f8a8fbd3f0550fc5038f1daaac90cf01410
Rafael Vicio <rafaelvicio@icloud.com>
2017-12-28 23:59:21 -0200

    Bean Validator e Convert Data

--- src/main/java/br/com/casadocodigo/loja/models/Livro.java
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -2,6 +2,7 @@ package br.com.casadocodigo.loja.models;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -10,6 +11,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 public class Livro {
@@ -17,11 +27,22 @@ public class Livro {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
+	@NotBlank
 	private String titulo;
+	
 	@Lob
+    @Length(min=10) // Número mínimo de caracteres que o campo pode ter
+    @NotBlank
 	private String descricao;
+	
+	@DecimalMin("20") // Valor decimal mínimo
 	private BigDecimal preco;
+	
+	@Min(50) // Valor inteiro mínimo
 	private Integer numeroPaginas;
+	
+	@Temporal(TemporalType.DATE)
+	private Calendar dataPublicacao = Calendar.getInstance(); // Pegamos uma instância de Calendar
 	
 	@ManyToMany
 	private List<Autor> autores = new ArrayList<>();
@@ -57,10 +78,17 @@ public class Livro {
 		this.autores = autores;
 	}
 	
+	public Calendar getDataPublicacao() {
+		return dataPublicacao;
+	}
+	public void setDataPublicacao(Calendar dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+	
 	@Override
 	public String toString() {
-		return "Livro [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + ", preco=" + preco
-				+ ", numeroPaginas=" + numeroPaginas + ", autores=" + autores + "]";
+	        return "Livro [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + ", preco=" + preco
+	                        + ", numeroPaginas=" + numeroPaginas + ", autores=" + autores + "]";
 	}
 
 	
